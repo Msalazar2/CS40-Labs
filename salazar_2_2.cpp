7596b68c47fd8a0a02ba2116e948e0dcf59230b8
Marcelino Salazar <marcelinosalazar2@gmail.com>
2024-09-05 20:21:48 -0700

    uploading lab 2.2

---
+++ salazar_2_2.cpp
@@ -0,0 +1,29 @@
+// This program will output the circumference and area
+// of the circle with a given radius.
+
+// PLACE YOUR NAME HERE
+
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+const double RADIUS = 5.4;
+
+int main()
+{
+	float area;								// definition of area of circle 
+	int circumference;				// definition of circumference 
+	
+    circumference = 2 * PI * RADIUS;	// computes circumference
+	area = 91.5624;							// computes area
+
+	cout << "The circumference of the circle is " << circumference << endl;
+    cout << "The area of the circle is " << area << endl;
+
+	return 0;
+}
+
+// Initially produces a syntax error due to missing semicolon, missing definition for area, and missing expression for area.
+// I changed circumference defintion to integer and added float to area
+// The output value of circumference is a discrete integer and no longer a floating point.
+// I input the value for area
